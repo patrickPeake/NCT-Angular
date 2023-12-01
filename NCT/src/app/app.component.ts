7bc72e83f552977c37d6eadb07e5f2f27507a86c
Patrick Laptop <pjp3@sfu.ca>
2023-11-30 17:59:19 -0800

    add button presses passed up

--- NCT/src/app/app.component.ts
+++ NCT/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NCT';
+
+  addInc(){
+    console.log("add")
+  }
 }
--- NCT/src/app/components/big-btn/big-btn.component.ts
+++ NCT/src/app/components/big-btn/big-btn.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-big-btn',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './big-btn.component.css'
 })
 export class BigBtnComponent {
-
+  @Output() btnClick = new EventEmitter()
+  onClick(){
+    this.btnClick.emit()
+  }
 }
