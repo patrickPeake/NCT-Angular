69b635e7c6e1425a8a59bec76540176cdf33062a
Patrick Laptop <pjp3@sfu.ca>
2023-11-30 21:26:24 -0800

    delete works

--- NCT/src/app/components/table/table.component.ts
+++ NCT/src/app/components/table/table.component.ts
@@ -13,19 +13,49 @@ export class TableComponent implements OnInit {
   i = new Date().getTime()
 
   data: any[] = [];
+  firstValues: any[] = [];
+  resultArray: any[] = [];
 
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
     this.http.get<any[]>('https://272.selfip.net/apps/t4foZFvfjT/collections/people/documents/')
       .pipe(
+        map((jsonData: any[]) => {
+          return jsonData.map(item => {
+            const firstKey = Object.keys(item)[0]; // Extract the first key
+            const firstValue = item[firstKey]; // Extract the value corresponding to the first key
+            return firstValue;
+          });
+        })
+      )
+      .subscribe((valuesArray: any[]) => {
+        this.firstValues = valuesArray;
+        //console.log(this.firstValues);
+      });
+
+    this.http.get<any[]>('https://272.selfip.net/apps/t4foZFvfjT/collections/people/documents/')
+      .pipe(
         map((jsonData: any[]) => (jsonData as any[]).map(item => item.data))
       )
       .subscribe((dataArray: any[]) => {
         this.data = dataArray;
-        console.log(this.data);
-
+        //console.log(this.data);
+        for (let j = 0; j < this.data.length; j++) {
+          let newArray = [ this.firstValues[j]];
+          for(let k=0; k<this.data[j].length; k++){
+            newArray.push(this.data[j][k]);
+            //console.log("this.data[j][k]");
+          }
+          this.resultArray.push(newArray);
+          //console.log(this.resultArray);
+        }
+        this.data = this.resultArray;
+        console.log(this.resultArray);
+        console.log("end");
       });
+
+      
   }
 
     
@@ -34,4 +64,16 @@ export class TableComponent implements OnInit {
 
 
   headers = ['Location', 'Name', 'Time', 'Status', 'more info', 'delete']; // Add or remove headers as needed
+
+
+  deleteRow(rowId: any): void {
+    // Implement the logic to delete the row with the given ID
+    console.log('Deleting row with ID:', rowId);
+    // Add your deletion logic here
+    const toDel = 'https://272.selfip.net/apps/t4foZFvfjT/collections/people/documents/' + rowId + '/';
+    //console.log(toDel);
+    this.http.delete(toDel,{}).subscribe()
+    window.location.reload();
+  }
+
 }
